40d0a847d46e9efc289fdf1e1ccab528ebf51445
nitya singh <nityasingh559@gmail.com>
2025-08-11 16:38:21 +0530

    add code

--- index.js
+++ index.js
@@ -2,21 +2,52 @@ const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
+async function main() {
+  const db = await open({
+    filename: 'chat.db',
+    driver: sqlite3.Database
+  });
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
-});
+  
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+    );
+  `);
 
-io.on('connection',(socket) => {
-    socket.on('chat message',(msg) => {
-       io.emit('chat message', msg);
+  const app = express();
+  const server = createServer(app);
+  const io = new Server(server, {
+    connectionStateRecovery: {}
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+  });
+
+  io.on('connection', (socket) => {
+    socket.on('chat message', async (msg) => {
+      let result;
+      try {
+        
+        result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+      } catch (e) {
+        
+        return;
+      }
+      
+      io.emit('chat message', msg, result.lastID);
     });
-});
+  });
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
-});
+  server.listen(3000, () => {
+    console.log('server running at http://localhost:3000');
+  });
+}
+
+main();
